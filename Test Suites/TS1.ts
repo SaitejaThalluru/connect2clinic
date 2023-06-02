04b39295abc1d5827a3bfb1f7fcdc02844580e8f
Thalluru Saiteja <thalluru.s@mantralabsglobal.com>
2023-06-02 08:19:21 +0530

    Added Test Suite

---
+++ Test Suites/TS1.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>e12105d6-ec9e-456c-a436-af02f83b6c58</testSuiteGuid>
+</TestSuiteEntity>
